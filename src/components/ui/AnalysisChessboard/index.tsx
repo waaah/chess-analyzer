4caae8b3916a6ad6a755a294b6443fbacc23d498
Joshua Co <cojosh90@gmail.com>
2025-05-26 22:59:18 +0800

    feat: add alternate moves tree

--- src/components/ui/AnalysisChessboard/index.tsx
+++ src/components/ui/AnalysisChessboard/index.tsx
@@ -3,80 +3,88 @@ import { Chess, Move, Square } from "chess.js";
 import React, { useEffect, useRef } from "react";
 import { Chessboard } from "react-chessboard";
 
-export const AnalysisChessboard: React.FC<{ game: Chess }> = (props) => {
-    const [fen, setFen] = React.useState<string>("");
-    const { game } = props;
-    const moveSound = useRef<HTMLAudioElement>(null);
-    const moveCheckSound = useRef<HTMLAudioElement>(null);
-    const gameOverSound = useRef<HTMLAudioElement>(null);
-    const takesSound = useRef<HTMLAudioElement>(null);
-    const castleSound = useRef<HTMLAudioElement>(null);
-    const illegalMoveSound = useRef<HTMLAudioElement>(null);
+export const AnalysisChessboard: React.FC<{
+  game: Chess;
+  setCurrentMove: (sourceSquare: string, prevSquare: string) => void;
+}> = (props) => {
+  const [fen, setFen] = React.useState<string>("");
+  const { game, setCurrentMove } = props;
+  const moveSound = useRef<HTMLAudioElement>(null);
+  const moveCheckSound = useRef<HTMLAudioElement>(null);
+  const gameOverSound = useRef<HTMLAudioElement>(null);
+  const takesSound = useRef<HTMLAudioElement>(null);
+  const castleSound = useRef<HTMLAudioElement>(null);
+  const illegalMoveSound = useRef<HTMLAudioElement>(null);
 
-    useEffect(() => {
-        // Initialize audio object
-        moveSound.current = new Audio("/sounds/move-self.mp3");
-        gameOverSound.current = new Audio("/sounds/game-end.webm");
-        castleSound.current = new Audio("/sounds/castle.mp3");
-        takesSound.current = new Audio("/sounds/capture.mp3");
-        moveCheckSound.current = new Audio("/sounds/move-check.mp3");
-        illegalMoveSound.current = new Audio("/sounds/illegal.mp3");
+  useEffect(() => {
+    // Initialize audio object
+    moveSound.current = new Audio("/sounds/move-self.mp3");
+    gameOverSound.current = new Audio("/sounds/game-end.webm");
+    castleSound.current = new Audio("/sounds/castle.mp3");
+    takesSound.current = new Audio("/sounds/capture.mp3");
+    moveCheckSound.current = new Audio("/sounds/move-check.mp3");
+    illegalMoveSound.current = new Audio("/sounds/illegal.mp3");
 
-        return () => {
-            moveSound.current?.remove();
-            moveCheckSound.current?.remove();
-            gameOverSound.current?.remove();
-            takesSound.current?.remove();
-            castleSound.current?.remove();
-            illegalMoveSound.current?.remove();
-        }
-    }, []);
+    return () => {
+      moveSound.current?.remove();
+      moveCheckSound.current?.remove();
+      gameOverSound.current?.remove();
+      takesSound.current?.remove();
+      castleSound.current?.remove();
+      illegalMoveSound.current?.remove();
+    };
+  }, []);
 
-    useEffect(() => {
-        if (game) {
-            const history = game.history({ verbose: true });
-            if (history.length > 0) {
-                setGameState(history[history.length - 1]);
-            }
-            else {
-                setFen(game.fen());
-            }
-        }
-    }, [game])
+  useEffect(() => {
+    if (game) {
+      const history = game.history({ verbose: true });
+      if (history.length > 0) {
+        setGameState(history[history.length - 1]);
+      } else {
+        setFen(game.fen());
+      }
+    }
+  }, [game]);
 
-    const onDrop = (sourceSquare: Square, targetSquare: Square) => {
-        const move = game.move({ from: sourceSquare, to: targetSquare });
-        return setGameState(move);
+  const onDrop = (sourceSquare: Square, targetSquare: Square) => {
+    const move = game.move({ from: sourceSquare, to: targetSquare });
+    const validState = setGameState(move);
+
+    if (validState) {
+      const history = game.history();
+      const prevMove = history[history.length - 2];
+      const currentMove = history[history.length - 1];
+      setCurrentMove(prevMove, currentMove);
     }
 
-    const setGameState = (move: Move) => {
-        try {
-            if (game.isCheckmate() || game.isDraw())
-                gameOverSound.current?.play();
-            else if (move.isKingsideCastle() || move.isQueensideCastle())
-                castleSound.current?.play();
-            else if (move.isCapture())
-                takesSound.current?.play();
-            else if (move.isPromotion())
-                moveSound.current?.play();
-            else if (move.san.endsWith("+"))
-                moveCheckSound.current?.play();
-            else
-                moveSound.current?.play();
+    return validState;
+  };
 
-            setFen(game.fen());
-            return true;
-        } catch (error) {
-            console.error(error);
-            illegalMoveSound.current?.play();
-            return false;
-        }
+  const setGameState = (move: Move) => {
+    try {
+      if (game.isCheckmate() || game.isDraw()) gameOverSound.current?.play();
+      else if (move.isKingsideCastle() || move.isQueensideCastle())
+        castleSound.current?.play();
+      else if (move.isCapture()) takesSound.current?.play();
+      else if (move.isPromotion()) moveSound.current?.play();
+      else if (move.san.endsWith("+")) moveCheckSound.current?.play();
+      else moveSound.current?.play();
+
+      setFen(game.fen());
+      return true;
+    } catch {
+      illegalMoveSound.current?.play();
+      return false;
     }
-    return <>
-        <Chessboard
-            arePiecesDraggable={true}      // ✅ Enable dragging 
-            position={fen}
-            onPieceDrop={onDrop}
-            boardWidth={560} />
-    </>;
+  };
+  return (
+    <>
+      <Chessboard
+        arePiecesDraggable={true} // ✅ Enable dragging
+        position={fen}
+        onPieceDrop={onDrop}
+        boardWidth={560}
+      />
+    </>
+  );
 };
--- src/components/ui/MovesTable/index.tsx
+++ src/components/ui/MovesTable/index.tsx
@@ -1,50 +1,79 @@
-import { Box, Table, Text } from "@radix-ui/themes"
+import { MoveTree } from "@/hooks/useChess";
+import { Box, Table, Text } from "@radix-ui/themes";
+import { MovesList } from "../MovesList";
 
 export const MovesTable: React.FC<{
-    history: string[];
-    currentMove: number;
-    setCurrentMove: (n: number) => void;
+  moveTree: MoveTree[];
+  currentMove: number;
+  selectCurrentMove: (n: number) => void;
 }> = (props) => {
-    const { history, setCurrentMove, currentMove } = props;
-    return <Box className="max-h-100 overflow-y-auto border border-gray-300">
-        <Table.Root>
-            <Table.Header>
-                <Table.Row>
-                    <Table.ColumnHeaderCell className="text-xs">#</Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-                </Table.Row>
-            </Table.Header>
-            <Table.Body>
-                {
-                    new Array(Math.ceil(history.length / 2)).fill(0).map((_, index) => {
-                        const isWhiteActive = index * 2 + 1 === currentMove;
-                        const isBlackActive = index * 2 + 2 === currentMove;
+  const { moveTree, selectCurrentMove, currentMove } = props;
+  return (
+    <Box className="max-h-100 overflow-y-auto border border-gray-300">
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell className="text-xs">
+              #
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {new Array(Math.ceil(moveTree.length / 2)).fill(0).map((_, index) => {
+            const isWhiteActive = index * 2 + 1 === currentMove;
+            const isBlackActive = index * 2 + 2 === currentMove;
+            const activeClass = "rounded-sm bg-gray-200 ";
+            const whiteMoves = moveTree[index * 2];
+            const blackMoves = moveTree[index * 2 + 1];
 
-                        const activeClass = "rounded-sm bg-gray-200 ";
-                        return <Table.Row key={index}>
-                            <Table.RowHeaderCell className="text-xs">{index + 1}</Table.RowHeaderCell>
-                            <Table.Cell className="text-xs">
-                                <Text
-                                    weight={isWhiteActive ? "bold" : "regular"}
-                                    className={`cursor-pointer p-1 ${isWhiteActive ? activeClass : ""}`}
-                                    onClick={() => setCurrentMove(index * 2 + 1)}
-                                >
-                                    {history[index * 2]}
-                                </Text>
-                            </Table.Cell>
-                            <Table.Cell className="text-xs">
-                                <Text
-                                    weight={isBlackActive ? "bold" : "regular"}
-                                    className={`cursor-pointer p-1 ${isBlackActive ? activeClass : ""}`}
-                                    onClick={() => setCurrentMove(index * 2 + 2)}>
-                                    {history[index * 2 + 1]}
-                                </Text>
-                            </Table.Cell>
-                        </Table.Row>
-                    })
-                }
-            </Table.Body>
-        </Table.Root>
+            return (
+              <Table.Row key={index}>
+                <Table.RowHeaderCell className="text-xs">
+                  {index + 1}
+                </Table.RowHeaderCell>
+                <Table.Cell className="text-xs">
+                  <Text
+                    weight={isWhiteActive ? "bold" : "regular"}
+                    className={`cursor-pointer p-1 ${
+                      isWhiteActive ? activeClass : ""
+                    }`}
+                    onClick={() => selectCurrentMove(index * 2 + 1)}
+                  >
+                    {moveTree[index * 2].square}
+                  </Text>
+                </Table.Cell>
+                <Table.Cell className="text-xs">
+                  {blackMoves ? (
+                    <Text
+                      weight={isBlackActive ? "bold" : "regular"}
+                      className={`cursor-pointer p-1 ${
+                        isBlackActive ? activeClass : ""
+                      }`}
+                      onClick={() => selectCurrentMove(index * 2 + 2)}
+                    >
+                      {moveTree[index * 2 + 1].square}
+                    </Text>
+                  ) : (
+                    <></>
+                  )}
+                  {moveTree[index * 2].alternates.length > 0 ? (
+                    <MovesList movesTree={whiteMoves.alternates} />
+                  ) : (
+                    <></>
+                  )}
+                  {blackMoves && blackMoves.alternates.length > 0 ? (
+                    <MovesList movesTree={blackMoves.alternates} />
+                  ) : (
+                    <></>
+                  )}
+                </Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table.Root>
     </Box>
-}
+  );
+};
